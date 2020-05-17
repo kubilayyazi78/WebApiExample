0fb31026c381721b898cd6b571ae11551014ebc3
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-05-18 02:23:51 +0300

    Veriler JSON ile listelendi

---
+++ Asp.NetWebAPI/Controllers/PersonellerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asp.NetWebAPI.Areas.HelpPage.Controllers
+{
+    public class PersonellerController : Controller
+    {
+        // GET: HelpPage/Personeller
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TumPersoneller()
+        {
+            return View();
+        }
+    }
+}
